ac4b024212059fe1bcb8d3f98513f5fb38e89cd8
armgits <126507842+armgits@users.noreply.github.com>
2023-10-25 00:47:33 +0000

    Backlog 2.1.3 - Declared public members of IK class

--- libs/Inverse/Inverse.hpp
+++ libs/Inverse/Inverse.hpp
@@ -16,6 +16,16 @@
 class Inverse {
 
  public:
+  Inverse();
+  std::vector<double> inverse(std::vector<double> target_coordinates,
+                              std::vector<double> initial_pose);
+  std::vector<double> get_input_coordinates();
+  void set_input_coordinates(std::vector<double> input_coords);
+  std::vector<double> get_robot_pose();
+  void set_robot_pose(std::vector<double> robot_pose);
+  std::vector<double> get_angle_constraint();
+  void set_angle_constraint(std::vector<double> angle_constraints);
+  double **get_dh_params();
 
  private:
   std::vector<double> input_coordinates_;
