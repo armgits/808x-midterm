2e8d5014ede6eb156d31128a37fae2a13491ec2a
armgits <126507842+armgits@users.noreply.github.com>
2023-10-25 01:14:16 +0000

    Written dummy implementation of forward class for test cases

--- libs/Forward/src.cpp
+++ libs/Forward/src.cpp
@@ -8,3 +8,14 @@
  * @copyright Copyright (c) 2023 Group 12
  *
  */
+
+#include "Forward.hpp"
+
+Forward::Forward() : input_angles_{}, output_angles_{}, robot_tcp_position_{},
+                     coordinate_constraints_{}, dh_params_{} {}
+
+std::vector<double> Forward::forward(
+    std::vector<double> joint_angles, std::vector<double> tcp_position) {
+  std::vector<double> dummy_vector {1.0};
+  return dummy_vector;
+}
