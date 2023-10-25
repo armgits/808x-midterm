04f2cf0df3adfee74745d52797f54cfd297e2e63
Abhimanyu Saxena <abhi.manyu1225@gmail.com>
2023-10-24 22:50:35 -0400

    Added author info

--- test/main.cpp
+++ test/main.cpp
@@ -1,6 +1,17 @@
+/**
+ * @file main.cpp
+ * @author Abhimanyu Saxena (asaxena4@umd.edu)
+ * @brief File to run test cases for Inverse and Forward classes
+ * @version 0.1
+ * @date 2023-10-24
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
 #include <gtest/gtest.h>
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
