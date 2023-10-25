c34df72e0411ab61ce4a387866cc141174725ea8
armgits <126507842+armgits@users.noreply.github.com>
2023-10-24 20:09:34 -0400

    Backlog 2.2 - Created header and source files for FK class

---
+++ libs/Forward/Forward.hpp
@@ -0,0 +1,12 @@
+/**
+ * @file Forward.hpp
+ * @author Abhishekh Reddy (areddy42@umd.edu)
+ * @brief Header file for Forward kinematics library declarations
+ * @version 0.1
+ * @date 2023-10-20
+ *
+ * @copyright Copyright (c) 2023 Group 12
+ *
+ */
+ 
+#pragma once
---
+++ libs/Forward/src.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file src.cpp
+ * @author Abhishekh Reddy (areddy42@umd.edu)
+ * @brief Source file for Forward kinematics library definitions
+ * @version 0.1
+ * @date 2023-10-20
+ *
+ * @copyright Copyright (c) 2023 Group 12
+ *
+ */
