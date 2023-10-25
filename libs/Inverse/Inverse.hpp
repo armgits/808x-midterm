dc35c3afd0b67cb36ed03a9c4d3fa65a572c4a54
armgits <126507842+armgits@users.noreply.github.com>
2023-10-24 20:04:30 -0400

    Backlog 2.1 - Created header and source files for IK class

---
+++ libs/Inverse/Inverse.hpp
@@ -0,0 +1,12 @@
+/**
+ * @file Inverse.hpp
+ * @author Abhishekh Reddy (areddy42@umd.edu)
+ * @brief Header file for Inverse kinematics library declarations
+ * @version 0.1
+ * @date 2023-10-20
+ *
+ * @copyright Copyright (c) 2023 Group 12
+ *
+ */
+
+#pragma once
