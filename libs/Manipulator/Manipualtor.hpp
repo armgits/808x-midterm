af90cb2e04f8f60497415d0d36013d10518fc1ef
armgits <126507842+armgits@users.noreply.github.com>
2023-10-24 20:10:12 -0400

    Backlog 2.3 - Created header and source files for Manipulator class

---
+++ libs/Manipulator/Manipualtor.hpp
@@ -0,0 +1,12 @@
+/**
+ * @file Manipualtor.hpp
+ * @author Abhishekh Reddy (areddy42@umd.edu)
+ * @brief Header file for Manipulator library declarations
+ * @version 0.1
+ * @date 2023-10-20
+ *
+ * @copyright Copyright (c) 2023 Group 12
+ *
+ */
+
+#pragma once
