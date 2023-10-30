ad39625df2cc1765c68e1f20318bc9c8b2d0d1d9
armgits <126507842+armgits@users.noreply.github.com>
2023-10-30 23:34:16 +0000

    Updated src file to include revamped inverse library

--- libs/klib-inverse/src.cpp
+++ libs/klib-inverse/src.cpp
@@ -9,27 +9,27 @@
  *
  */
 
-#include "Inverse.hpp"
+#include "klib-inverse.hpp"
 
-/**
-* @brief Constructor for inverse kinematics class. Initializes attributes to zero.
-*
-*/
-Inverse::Inverse() : input_coordinates_{}, output_coordinates_{}, robot_pose_{},
-                    angle_constraints_{}, dh_params_{} {}
+// /**
+// * @brief Constructor for inverse kinematics class. Initializes attributes to zero.
+// *
+// */
+// Inverse::Inverse() : input_coordinates_{}, output_coordinates_{}, robot_pose_{},
+//                     angle_constraints_{}, dh_params_{} {}
 
-/**
- * @brief Method to compute the inverse kinematics for given target end-effector
- *         coordinate. Currently returns a dummy vector. Implementation pending.
- *
- * @param target_coordinates
- * @param initial_pose
- * @return std::vector<double>
- */
-std::vector<double> Inverse::inverse(
-    const std::vector<double>& target_coordinates,
-    const std::vector<double>& initial_pose) {
-  std::vector<double> dummy_vector {1.0};
+// /**
+//  * @brief Method to compute the inverse kinematics for given target end-effector
+//  *         coordinate. Currently returns a dummy vector. Implementation pending.
+//  *
+//  * @param target_coordinates
+//  * @param initial_pose
+//  * @return std::vector<double>
+//  */
+// std::vector<double> Inverse::inverse(
+//     const std::vector<double>& target_coordinates,
+//     const std::vector<double>& initial_pose) {
+//   std::vector<double> dummy_vector {1.0};
 
-  return dummy_vector;
-}
+//   return dummy_vector;
+// }
