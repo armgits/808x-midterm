ccf970a320010d49f01003b558b781b79b0ae8cb
armgits <126507842+armgits@users.noreply.github.com>
2023-10-30 23:30:29 +0000

    Backlog 2.1.4 - Updated Inverse library to the new class structure

--- libs/klib-inverse/klib-inverse.hpp
+++ libs/klib-inverse/klib-inverse.hpp
@@ -13,6 +13,8 @@
 
 #include <vector>
 
+#include "klib-datatypes.hpp"
+
 /**
  * @brief Class for computing the inverse kinematics of an articulated arm
  *
@@ -23,31 +25,17 @@ class Inverse {
    * @brief Constructor for an inverse kinematics solver object
    *
    */
-  Inverse();
+  Inverse(const klib::DHParameters6R arm_description_);
 
   /**
    * @brief Method to compute the inverse kinematics with the solver object
    *
    * @param target_coordinates
-   * @param initial_pose
+   * @param current_pose
    * @return std::vector<double>
    */
-  std::vector<double> inverse(const std::vector<double>& target_coordinates,
-                              const std::vector<double>& initial_pose);
-
-  /**
-   * @brief Method to obtain the target coordinates for the end-effector
-   *
-   * @return std::vector<double>
-   */
-  std::vector<double> get_input_coordinates();
-
-  /**
-   * @brief Method to set the target coordinates for the end-effector
-   *
-   * @param input_coords
-   */
-  void set_input_coordinates(const std::vector<double>& input_coords);
+  klib::ArmPose6R compute(const klib::Point& target_coordinates,
+                          const klib::ArmPose6R& current_pose);
 
   /**
    * @brief Method to obtain the joint angles describing the robot pose computed
@@ -57,66 +45,22 @@ class Inverse {
    */
   std::vector<double> get_robot_pose();
 
-  /**
-   * @brief Method to manually override the robot pose computed by the inverse
-   *        kinematics solver
-   *
-   * @param robot_pose
-   */
-  void set_robot_pose(const std::vector<double>& robot_pose);
-
-  /**
-   * @brief Method to obtain the joint angles constraints set for the arm and
-   *        for generating an inverse kinematics solution.
-   *
-   * @return std::vector<double>
-   */
-  std::vector<double> get_angle_constraint();
-
-  /**
-   * @brief Method to set the joint angles constraints to to generate a valid
-   *        inverse kinematics solution.
-   *
-   * @param angle_constraints
-   */
-  void set_angle_constraint(const std::vector<double>& angle_constraints);
-
-  /**
-   * @brief Method to obtain the DH Parameters describing the arm for computing
-   *        inverse kinematics.
-   *
-   * @return double**
-   */
-  double **get_dh_params();
-
  private:
   /**
    * @brief Stores the target coordinates for the end-effector
    *
    */
-  std::vector<double> input_coordinates_;
-
-  /**
-   * @brief Stores the final coordinates achieved by the solver object
-   *
-   */
-  std::vector<double> output_coordinates_;
+  klib::Point input_coordinates_;
 
   /**
    * @brief Stores the pose computed to achieve target end-effector coordinates
    *
    */
-  std::vector<double> robot_pose_;
-
-  /**
-   * @brief Stores the joint angle constraints for generating a valid solution
-   *
-   */
-  std::vector<double> angle_constraints_;
+  klib::ArmPose6R robot_pose_;
 
   /**
    * @brief Stores the DH Parameters that describes the arm
    *
    */
-  double **dh_params_;
+  klib::DHParameters6R dh_params_;
 };
