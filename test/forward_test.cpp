d3e6532793a5031caeb260b632a80016f72f6080
Abhimanyu Saxena <abhi.manyu1225@gmail.com>
2023-10-31 23:49:05 -0400

    Backlog 2.2.4.3 - Added file for forward kinematics test

---
+++ test/forward_test.cpp
@@ -0,0 +1,63 @@
+/**
+ * @file test.cpp
+ * @author Abhimanyu Saxena (asaxena4@umd.edu)
+ * @brief Adding dummy test cases for Sprint 1
+ * @version 0.1
+ * @date 2023-10-31
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#include <gtest/gtest.h>
+
+#include "klib-datatypes.hpp"
+#include "klib-forward.hpp"
+#include "klib-manipulator.hpp"
+
+const double pi = 3.141592;
+const double epsilon = 0.5;
+
+TEST(forward_kinematics_test_1, should_return_tcp_pose) {
+  // Create an instance of manipulator class
+  Manipulator manipulator;
+  Forward f1;
+
+  // Define the expected end effector pose for a set of known joint angles
+  std::vector<double> joint_angles = {0.0,     -pi / 2, -pi / 2,
+                                      -pi / 2, pi / 2,  0.0012};
+  klib::Pose expected_pose{0.49309, -0.13026, 0.48946, 2.218, -2.216, 0.012};
+
+  // Call the forward kinematics method to get the actual end effector pose
+  klib::Pose tcp_pose = f1.forward(joint_angles);
+
+  // Check if the actual pose is equal to the expected pose within a tolerance
+  EXPECT_NEAR(tcp_pose.x, expected_pose.x, epsilon);
+  EXPECT_NEAR(tcp_pose.y, expected_pose.y, epsilon);
+  EXPECT_NEAR(tcp_pose.z, expected_pose.z, epsilon);
+  EXPECT_NEAR(tcp_pose.wx, expected_pose.wx, epsilon);
+  EXPECT_NEAR(tcp_pose.wy, expected_pose.wy, epsilon);
+  EXPECT_NEAR(tcp_pose.wz, expected_pose.wz, epsilon);
+}
+
+TEST(forward_kinematics_test_1, should_return_tcp_pose) {
+  // Create an instance of manipulator class
+  Manipulator manipulator;
+  Forward f1;
+
+  // Define the expected end effector pose for a set of known joint angles
+  std::vector<double> joint_angles = {pi / 2,  -pi / 2, -pi / 2,
+                                      -pi / 2, pi / 2,  0.0012};
+  klib::Pose expected_pose{0.13150, 0.492, 0.48944, pi, -0.002, 0.015};
+
+  // Call the forward kinematics method to get the actual end effector pose
+  klib::Pose tcp_pose = f1.forward(joint_angles);
+
+  // Check if the actual pose is equal to the expected pose within a tolerance
+  EXPECT_NEAR(tcp_pose.x, expected_pose.x, epsilon);
+  EXPECT_NEAR(tcp_pose.y, expected_pose.y, epsilon);
+  EXPECT_NEAR(tcp_pose.z, expected_pose.z, epsilon);
+  EXPECT_NEAR(tcp_pose.wx, expected_pose.wx, epsilon);
+  EXPECT_NEAR(tcp_pose.wy, expected_pose.wy, epsilon);
+  EXPECT_NEAR(tcp_pose.wz, expected_pose.wz, epsilon);
+}
