f4856d34444b54bada7ea6819bb9fe640278fdcc
muditsingal <muditsingal@gmail.com>
2023-10-31 23:41:41 -0400

    Backlog 2.2.4.2 - Added test fire for Inverse kinematics class test

---
+++ test/inverse_test.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file inverse_test.cpp
+ * @author Mudit Singal (msingal@umd.edu)
+ * @brief Test cases for inverse kinematics class
+ * @version 0.1
+ * @date 2023-10-30
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#include <gtest/gtest.h>
+
+#include "klib-inverse.hpp"
+
+const double pi = 3.14159265;
